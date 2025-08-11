9f56dbea0b3a9874ee447a6c283d927a6257d918
Lucas Godina <lucasgodina@gmail.com>
2025-08-10 22:29:48 -0300

    Add email authentication backend and improve user registration email validation

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 
 from .models import Profile
 
+User = get_user_model()
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -14,7 +16,7 @@ class UserRegistrationForm(forms.ModelForm):
     password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ["username", "first_name", "email"]
 
     def clean_password2(self):
@@ -25,11 +27,24 @@ class UserRegistrationForm(forms.ModelForm):
 
         return cd["password2"]
 
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError("Email already in use.")
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ["first_name", "last_name", "email"]
+
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError("Email already in use.")
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
